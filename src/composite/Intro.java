305a2d0790968d74fe76f45cfa1298a4797b7a76
NewSong <songxinjianzx@163.com>
2016-11-26 22:30:32 +0800

    宋欣建 11/26 23种设计模式Done！

--- src/composite/Intro.java
+++ src/composite/Intro.java
@@ -1,11 +1,12 @@
 package composite;
 /**
- * 
+ * 组合模式
 * @ClassName: Intro
 * @Description: TODO
 * @author NewSong
 * @date 2016年11月26日 下午5:38:26
-*
+* 需要一种树形结构
+* 允许将对象组合成树形结构来表现 整体/部分层次结构；组合能让客户以一致的方式处理个别对象以及对象组合
  */
 public class Intro {
 
--- src/memento/Intro.java
+++ src/memento/Intro.java
@@ -1,5 +1,19 @@
 package memento;
+/**
+ * 备忘录模式（保存与恢复现场）
+* @ClassName: Intro
+* @Description: TODO
+* @author NewSong
+* @date 2016年11月26日 下午10:26:20
+* Originate是实体类，并负责创建和恢复Memento（比如JavaBean）
+  Memento负责保存对象的状态
+  CareTaker 负责存储Memento（一个或一系列）（多条历史记录）
 
+  Originate除了对象的属性和setter getter之外，还有创建和恢复Memento的方法
+  Memento也持有对象的所有属性和setter getter，它的构造方法是由Originate对象得到其内部状态。
+  CareTaker持有一个或一组Memento，并提供setter and getter
+
+ */
 public class Intro {
 
 }
